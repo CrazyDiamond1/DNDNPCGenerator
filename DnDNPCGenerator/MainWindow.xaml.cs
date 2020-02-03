78b2484578134f60ae5a99bebf62eb43dd495b69
Noah Tanner <ntanner@student.neumont.edu>
2020-02-03 05:23:10 -0700

    Separated page content from MainWindow.

--- DnDNPCGenerator/MainWindow.xaml.cs
+++ DnDNPCGenerator/MainWindow.xaml.cs
@@ -20,52 +20,11 @@ namespace DnDNPCGenerator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
-        public List<Character> Characters { get; private set; }
-        public Character SelectedCharacter { get; private set; }
-
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = SelectedCharacter;
-            Characters = new List<Character>();
-        }
-
-        private void GenerateButton_Click(object sender, RoutedEventArgs e)
-        {
-            SelectedCharacter = new Character();
-            DisplayCharacterStats();
-            Characters.Add(SelectedCharacter);
-            AddCharacterItemBoxToCharacterListBox(SelectedCharacter);
-        }
-
-        private void AddCharacterItemBoxToCharacterListBox(Character c)
-        {
-            ListViewControl newListView = new ListViewControl(c);
-            CharacterBox.Items.Add(newListView);
-        }
-
-        private void CharacterBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ListViewControl selectedCharacterItem = (ListViewControl)CharacterBox.Items.GetItemAt(CharacterBox.SelectedIndex);
-            SelectedCharacter = selectedCharacterItem.SetCharacter;
-            DisplayCharacterStats();
-        }
-
-        private void DisplayCharacterStats()
-        {
-            CharName.Content = SelectedCharacter.Name;
-            CharRace.Content = SelectedCharacter.Race;
-            CharGender.Content = SelectedCharacter.Gender;
-            CharAlignment.Content = SelectedCharacter.Alignment;
-            CharClass.Content = SelectedCharacter.DnDClass;
-            CharStr.Content = SelectedCharacter.Strength;
-            CharDex.Content = SelectedCharacter.Dexterity;
-            CharCon.Content = SelectedCharacter.Constitution;
-            CharInt.Content = SelectedCharacter.Intelligence;
-            CharWis.Content = SelectedCharacter.Wisdom;
-            CharChr.Content = SelectedCharacter.Charisma;
         }
     }
 }
